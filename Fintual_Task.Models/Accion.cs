d90b46c436a2b4bdbc775034ca6ab9e28664f4fa
Bastian <bspuler@openrad.cl>
2025-06-13 21:40:26 -0400

    Add: Agrega capa models

---
+++ Fintual_Task.Models/Accion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fintual_Task.Models
+{
+    public class Accion
+    {
+        public string Nombre { get; set; }
+        public decimal PrecioActual { get; set; }
+        public decimal Cantidad { get; set; }
+
+        public Accion(string nombre, decimal precio, decimal cantidad)
+        {
+            Nombre = nombre;
+            PrecioActual = precio;
+            Cantidad = cantidad;
+        }
+
+        public decimal GetValor()
+        {
+            return PrecioActual * Cantidad;
+        }
+    }
+
+}
